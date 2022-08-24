750a93dfde2db45301feecdd1bfb4eb33deffaac
Fernanda Winter <fernanda.winter@hotmail.com>
2022-08-24 18:37:00 -0300

    deleting console.log's and content.hover

--- src/api.ts
+++ src/api.ts
@@ -3,10 +3,8 @@ import axios from 'axios';
 const baseUrl = 'https://www.breakingbadapi.com';
 
 export async function fetchRandomQuote() {
-  console.log('5 - entrou no getRandomQuote')
   const endpoint = '/api/quote/random'
   const url = baseUrl + endpoint;
   const response: any = await axios.get(url);
-   console.log('7 - antes do return do getRandomQuote')
   return response.data[0]
 }
